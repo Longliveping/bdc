c088d7cf0d9258639ab1104aa547fe5b214e88f2
Longliveping <liping.long@me.com>
2021-01-03 12:08:47 +0800

    add .env load

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(env_path)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,13 +22,15 @@ class Config:
 class TestingConfig(Config):
     """Configurations for Testing."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'bdc-testing.sqlite')
     DEBUG = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'venv/bdc-dev.sqlite')
+                              'sqlite:///' + os.path.join(basedir, 'bdc-dev.sqlite')
+
 
 config = {
     'development': DevelopmentConfig,
