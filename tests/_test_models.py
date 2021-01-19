83a4ac7db19491d97e7ab571021ba8c94d23c8f7
longliveping <liping.long@me.com>
2021-01-19 18:09:10 +0800

    add testing files

---
+++ tests/_test_models.py
@@ -0,0 +1,62 @@
+import unittest
+from app import create_app, db
+from app.models import Word, Article, Sentence, SentenceWord, ArticleWord, SentenceReview, WordReview, MyWord, MySentence
+import os
+from utility.words import read_token_filename,get_tokens, read_text, read_file_by_name
+from app.controller import import_word, import_myword, import_sentence, import_article, import_articleword, DropTable, show_artile_words
+
+class WordsTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = create_app('testing')
+        cls.app_context = cls.app.app_context()
+        cls.app_context.push()
+
+    @classmethod
+    def tearDownClass(cls):
+        db.session.remove()
+        cls.app_context.pop()
+
+    # def test_import_article(self):
+    #     import_article('103_')
+    #     count = db.session.query(Article).count()
+    #     self.assertTrue(count>0)
+    #
+    # def test_import_big_txt(self):
+    #     import_word('103_')
+    #     count = db.session.query(Word).count()
+    #     self.assertTrue(count>0)
+    #
+    # def test_import_sentence(self):
+    #     import_word('103_')
+    #     import_sentence('103_')
+    #     count = db.session.query(Sentence).count()
+    #     self.assertTrue(count>0)
+    #
+    # def test_import_myword(self):
+    #     import_myword()
+    #     count = db.session.query(MyWord).count()
+    #     self.assertTrue(count>0)
+    #
+    # def test_import_articleword(self):
+    #     import_articleword('103_')
+    #     count = db.session.query(ArticleWord).count()
+    #     self.assertTrue(count>0)
+    #
+    # def test_show_artcle_word(self):
+    #     basename = os.path.basename(get_file_by_name('103_'))
+    #     filename = basename.split('.')[0]
+    #     show_artile_words(filename)
+
+
+    def test_query(self):
+        remove_tokens = ('he','is')
+        try:
+            db.session.query(MyWord).filter(MyWord.word.in_(remove_tokens)).delete(synchronize_session='fetch')
+            db.session.commit()
+        except :
+            print('error')
+
+
+
---
+++ tests/test_word_english.py
@@ -0,0 +1,54 @@
+import unittest
+import os
+from app import create_app
+from flask import current_app
+from utility.words import create_sentence_json,create_word_json,create_file_json,\
+    read_file_by_name, read_word_json_file, read_token_json,read_text, \
+    read_sentence_json_file,read_sentence_json, extract_text, \
+    read_file_by_type, read_sentence, create_sentence_english_json
+
+
+
+
+class WordsTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = create_app('testing')
+        cls.app_context = cls.app.app_context()
+        cls.app_context.push()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.app_context.pop()
+
+    def test_001_extract_text(self):
+        file = os.path.join(current_app.config.get('TESTING_FOLDER'), '103 The One With The Thumb.docx')
+        extract_text(file)
+
+    def test_002_create_sentence_json(self):
+        file = read_file_by_name('103')
+        basename = os.path.basename(file)
+        dirname = os.path.dirname(file)
+        json_file = os.path.join(dirname,basename.split('.')[0]+'_sentence.json')
+        create_sentence_english_json(file)
+        self.assertTrue(os.path.exists(json_file))
+
+    # @unittest.skip('it takes time')
+    def test_003_create_word_json(self):
+        file = read_file_by_name('103')
+        basename = os.path.basename(file)
+        dirname = os.path.dirname(file)
+        json_file = os.path.join(dirname,basename.split('.')[0]+'_word.json')
+        create_word_json(file)
+        self.assertTrue(os.path.exists(json_file))
+
+    def test_004_get_word_json(self):
+        json_file = read_word_json_file('103')
+        words = read_token_json(json_file)
+        self.assertTrue(len(words) == 738)
+
+    def test_005_get_sentence_json(self):
+        json_file = read_sentence_json_file('103')
+        sentences = read_sentence_json(json_file)
+        self.assertTrue(len(sentences) == 385)
